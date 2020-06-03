187bba8edc5e4b2f306dcdb2c18b93d02a023924
Alessandro9300 <aandreoli93@gmail.com>
2020-06-03 19:32:46 +0200

    messo sconto

--- index.php
+++ index.php
@@ -22,15 +22,29 @@
           $this -> peso = $peso;
         }
 
+        function sconto($sconto){
+
+          $scontato = $this -> prezzo - (($this -> prezzo * $sconto) / 100);
+
+          echo "Il prodotto " . $this -> nome . " scontato costa " . $scontato . " euro";
+        }
+
         function printMe(){
           echo "Il prodotto è: " . $this -> nome . ", appartiene alla tipologia " . $this -> tipologia . " costa " . $this -> prezzo . " euro al pezzo e pesa " . $this -> peso . " grammi al pezzo.";
         }
 
       }
 
-      $objPrimo = new Prodotto1("Coca cola", "alimentare", 3, 500);
-
+      $objPrimo = new Prodotto1("lasagne", "alimentare", 3, 500);
+      $objSecondo = new Prodotto1("olio motore", "fai-da-te", 20, 10000);
       $objPrimo -> printMe();
+      echo "<br>";
+      $objPrimo -> sconto(50);
+      echo "<br>";
+      $objSecondo -> printMe();
+      echo "<br>";
+      $objSecondo -> sconto(20);
+
 
 
      ?>
